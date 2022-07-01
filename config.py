f22630a7a2b120336e29f0dcd71125bb7a9d287d
Cole Ballard <cole.ballard97@gmail.com>
2022-07-01 10:19:25 -0500

    added starter files

---
+++ config.py
@@ -0,0 +1,23 @@
+# Dictionary file location, change if needed.
+dictionaryloc = 'data/dictionary.txt'
+
+# Turn text file location, change if needed.
+turntextloc = 'data/turntext.txt'
+
+# Wheel contents for file location, change if needed.
+wheeltextloc = 'data/wheeldata.txt'
+
+# End of Round Status status file, change if needed.
+roundstatusloc = 'data/roundstatus.txt'
+
+# number of rounds in a game
+maxrounds = 3
+
+# Vowel cost
+vowelcost = 250
+
+# final prize, you fill this in
+finalprize = 20000
+
+# final round, change if needed.
+finalRoundTextLoc = 'data/finalround.txt'
